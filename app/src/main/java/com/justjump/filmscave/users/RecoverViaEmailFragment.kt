c8e640d0700ad43ff4fa480a459e2fa4e77b16c2
Jorge Eliecer Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2021-04-10 21:52:42 +0100

    version code: 1.021

--- app/src/main/java/com/justjump/filmscave/users/RecoverViaEmailFragment.kt
+++ app/src/main/java/com/justjump/filmscave/users/RecoverViaEmailFragment.kt
@@ -6,11 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.justjump.filmscave.R
 import com.justjump.filmscave.databinding.FragmentRecoverViaEmailBinding
+
 import com.justjump.filmscave.users.viewmodel.RecoverViaEmailViewModel
 
-class RecoverViaEmailFragment : Fragment() {
+class RecoverViaEmailFragment : Fragment(), RecoverViaEmailViewModel.Message {
 
     private lateinit var binding: FragmentRecoverViaEmailBinding
     private lateinit var recoverViaEmailViewModel: RecoverViaEmailViewModel
@@ -29,10 +29,25 @@ class RecoverViaEmailFragment : Fragment() {
         recoverViaEmailViewModel = ViewModelProvider(this).get(RecoverViaEmailViewModel()::class.java)
 
         //********************************************************//
+        //          Event of reset password
+        //********************************************************//
+        binding.buttonSubmit.setOnClickListener {
+            if (binding.dataEmail.text!!.isNotEmpty()){
+                recoverViaEmailViewModel.emailValue.value = binding.dataEmail.text.toString()
+                binding.loading.visibility = View.VISIBLE
+                recoverViaEmailViewModel.resetPassword(this)
+            }
+        }
+
+        //********************************************************//
         //          Event to go back
         //********************************************************//
         binding.buttonBack.setOnClickListener {
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
+    }
+
+    override fun showMessage(message: Int, success: Boolean, fieldError: Int) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/justjump/filmscave/users/viewmodel/RecoverViaEmailViewModel.kt
+++ app/src/main/java/com/justjump/filmscave/users/viewmodel/RecoverViaEmailViewModel.kt
@@ -1,6 +1,16 @@
 package com.justjump.filmscave.users.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.justjump.filmscave.users.RecoverViaEmailFragment
 
 class RecoverViaEmailViewModel: ViewModel() {
+
+    interface Message{ fun showMessage(message: Int, success: Boolean, fieldError: Int) }
+
+    var emailValue = MutableLiveData<String>()
+
+    fun resetPassword(recoverViaEmailFragment: RecoverViaEmailFragment) {
+
+    }
 }
